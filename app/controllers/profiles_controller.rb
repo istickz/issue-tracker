179cc37a218357e68be680cb4610ef9eb82439bc
Temur Fatkulin <temur.fatkulin@gmail.com>
2013-07-02 11:23:42 +0600

    Change @user.id to string, for searh in pg db

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @issue = Issue.new
-    @issues_for_user = Issue.where(assigned: @user.id, solved: true)
+    @issues_for_user = Issue.where(assigned: "#{@user.id}", solved: true)
   end
 
 
